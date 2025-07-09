d229531b20e2349b69243418d6dec573de155447
Aneek Hazra <153304011+Itsme-Aneek@users.noreply.github.com>
2025-07-09 15:38:52 +0530

    updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="banner">
           <div className="left">ANEEK</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Kolkata, West Bengal</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
